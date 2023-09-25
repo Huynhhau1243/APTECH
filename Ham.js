b776e262b78f5cab60058432eb2deae3d4e9417b
Huynhhau1243 <haukhung1512005@gmail.com>
2023-09-25 18:45:00 +0700

    a

---
+++ Ham.js
@@ -0,0 +1,46 @@
+console.log('Xin chao cac ban')
+// xin chào các bạn
+//bài 1:
+// câu a:
+console.log('Hello world!')
+// câu b: 
+console.log("Huỳnh Hậu")
+//câu c: 
+console.clear()
+// câu d:
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+// câu e: 
+console.log('    *   ')
+console.log('   ***   ')
+console.log('  ****** ')
+console.log(' ********')
+console.log('**********')
+//câu f :
+console.log('  **    **  ')
+console.log('*    *     *')
+console.log('*          *')
+console.log('  *      *  ')
+console.log('      *       ')
+// câu 2:
+// câu a: 
+let a = 10;
+console.log(a)
+// câu b: 
+let b = 10;
+console.log(b)
+//câu c:
+a = a * 2
+console.log(a)
+// câu d :
+const pi = 3.14
+console.log(pi)
+// câu e: 
+// pi = 5 
+// console.log(pi)
+//câu f:
+ten = "Iphone 14"
+Hang_San_Xuat = "Apple"
